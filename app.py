d20a4394142d65ced4ad9d8a88edda346baa1778
MaryJena <jenamary966@gmail.com>
2024-09-09 21:13:20 +0530

    first commit

--- app.py
+++ app.py
@@ -1,35 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
 
-API_KEY = 'your_openweathermap_api_key'  # Replace with your API key
-BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
-
 @app.route('/')
 def index():
-    return render_template('index.html')
+    api_url = 'https://jsonplaceholder.typicode.com/posts'
+    response = requests.get(api_url)
+    data = response.json()
+    
+    # Pass the data to the template
+    return render_template('index.html', data=data)
 
-@app.route('/weather', methods=['POST'])
-def weather():
-    city = request.form.get('city')
-    if city:
-        response = requests.get(BASE_URL, params={
-            'q': city,
-            'appid': API_KEY,
-            'units': 'metric'
-        })
-        data = response.json()
-        if data['cod'] == 200:
-            weather_data = {
-                'city': data['name'],
-                'temperature': data['main']['temp'],
-                'description': data['weather'][0]['description']
-            }
-            return render_template('weather.html', weather=weather_data)
-        else:
-            return render_template('weather.html', error=data['message'])
-    return render_template('index.html', error='City is required')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5002)
